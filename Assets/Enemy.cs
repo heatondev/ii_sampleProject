18f9fe39f030978f502335aab5916a5d3be1645e
heatondev <35467354+heatondev@users.noreply.github.com>
2020-05-09 20:55:20 -0600

    Added a couple of objects to demonstraste collisions and destroy.gameObject - prefab Jaeger and Building. Created Enemy script and applied to Building and Robo

---
+++ Assets/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+       if (other.tag == "Jaeger")
+        {
+            Destroy(gameObject);
+            Debug.Log("Destroyed");
+        }
+            
+        
+    }
+}
